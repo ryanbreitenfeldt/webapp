1740e7c088f59ce2211457446be8f67677aaedbc
Ryan <ryanbreitenfeldt@gmail.com>
2021-07-25 13:31:44 -0700

    expanded wolf class

--- webapp/app/wolframalpha/wolf.py
+++ webapp/app/wolframalpha/wolf.py
@@ -1,6 +1,21 @@
 
+import requests
+from appid import appID
 
 class wolframAlpha():
     def __init__(self):
-        pass
+        self.appID = appID
+        self.urlbase = "http://api.wolframalpha.com/v1/simple?appid={}&i={}"
+        self.responce = None
     
+    
+    def stringConverter(self,input):
+        input = input.replace(" ","+")
+        input = input.replace("?", "%3F")
+        return input
+
+    def questionAssembler(self,input):
+        return self.urlbase.insert(self.appID,input)
+
+    def makeRequest(self,input):
+        self.responce = requests.get(input)
