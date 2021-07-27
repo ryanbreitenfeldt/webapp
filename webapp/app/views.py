be54c3486741e9a23f0085887078f058da135226
Ryan <ryanbreitenfeldt@gmail.com>
2021-07-26 18:27:26 -0700

    add view and a html

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from wolframalpha.wolf import wolframAlpha
 
 # Create your views here.
+
+
+def results(request):
+    x = wolframAlpha()
+    x.doItAll(request)
+    return render(request, "templates/results.html",{})
